5bf2873a3b706d88632122ca3b294f4cf53af27e
David SPORN <sporniket.studio@gmail.com>
2025-11-09 09:38:55 +0100

    Clone Hatari, compile and install it.

---
+++ crossdev-atarist/podman-prune.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Remove any untagged image
+echo "====[ BEFORE PRUNING ]===="
+podman image list --all
+
+echo
+echo "====[ PRUNING ]===="
+for img in $(podman image list --all | grep "<none>\\s*<none>" | grep -o "[0-9a-f]\{8,\}"); do
+    echo "remove image id $img..."
+    podman rmi $img
+done
+
+echo
+echo "====[ AFTER PRUNING ]===="
+podman image list --all
+
+echo
+echo "----{ DONE }----"
